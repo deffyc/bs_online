913c3ee5557a21dddc490894a13b5e4a9c7738a1
leozhang2018 <leozhang2018@gmail.com>
2016-03-02 22:29:20 +0800

    Remove Local config

--- librarian/dbcon.php
+++ librarian/dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-mysql_select_db('eb_lms',mysql_connect('localhost','demo','123456'))or die(mysql_error());
+mysql_select_db('eb_lms',mysql_connect('****','****','****'))or die(mysql_error());
 mysql_query("SET NAMES 'utf8'");
 mysql_query("SET CHARACTER_SET_CLIENT=utf8");
 mysql_query("SET CHARACTER_SET_RESULTS=utf8");
